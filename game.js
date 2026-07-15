676cd84b51372e49c1d6f5cf820e4322f78fb2f9
Krishna Sah <krishnasah8118@gmail.com>
2026-07-15 18:48:36 +0530

    Finally added javascrip logics

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turn0 =true;
 let winPatterns = [
@@ -13,10 +16,59 @@ let winPatterns = [
   [6, 7, 8]
 ];
 
+const resetGame = ()=>{
+  turn0 = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
 boxes.forEach((box) => {
   box.addEventListener("click",()=>{
-    console.log("box was clicked");
-    alert("ho");
-  })
+    if(turn0){
+      box.innerText="O";
+      turn0= false;
+    }else{
+      box.innerText="X";
+      turn0=true;
+    }
+    box.disabled=true;
+    checkWinner();
+  }); 
+  });
+  const disableBoxes = () => {
+    for(let box of boxes){
+      box.disabled = true;
+    }
+  };
+  
+  const enableBoxes = () => {
+    for(let box of boxes){
+      box.disabled = false;
+      box.innerText = "";
+    }
+  };
+   
+  const showWinner = (winner) =>{
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
 
-})
+
+
+const checkWinner = () => {
+     for(let pattern of winPatterns ) {
+      let pos1Val = boxes[pattern[0]].innerText;
+      let pos2Val = boxes[pattern[1]].innerText;
+      let pos3Val = boxes[pattern[2]].innerText;
+
+      if ( pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if( pos1Val === pos2Val && pos2Val === pos3Val) {
+          showWinner(pos1Val);
+        }
+      }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
